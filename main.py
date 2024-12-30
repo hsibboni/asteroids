55cf86e1a91cb6f4903ddac7d0df9caa8e501c05
hsibboni <hadrien.sibboni+gh@gmail.com>
2024-12-30 23:13:16 +0100

    Player is a white triangle

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import *
 
 
 
@@ -13,13 +14,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #All black
         screen.fill((0, 0, 0))
+        ###
+        player.draw(screen)
 
+
+        # flip (empty)
         pygame.display.flip()
+        dt_ms = clock.tick(60)
+        dt = dt_ms / 1000
 
 
 
